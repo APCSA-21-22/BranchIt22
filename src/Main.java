fe3fb285e9db5fd82481530235ba8f4975325545
MsRioux <jrioux@mvusd.net>
2022-05-13 07:58:32 -0700

    Initial commit with simple instructions

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.println("Hello CSA");
+        System.out.println("You will clone and branch this project. ");
+        System.out.println("Create a new class i.e.\"Alexa\'s Class\"");
+        System.out.println("Do something uniquely you in the class");
+        System.out.println("Then push it to Github");
+        System.out.println("We will then see how to merge.");
+    }
+}
